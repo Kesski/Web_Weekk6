4326ef59ca266b04ae230d70523f8317fe23ae62
Crystal Lyne~ <crystalrlyne@gmail.com>
2023-02-08 20:21:57 -0600

    Added code from tutor time

--- bridges.js
+++ bridges.js
@@ -1,9 +1,9 @@
-let bridgeCoordinates = [30, -95]
+let bridgeCoordinates = [30, -95] // sets the view of the map
 let zoomLevel = 3
 let map = L.map('bridge-map').setView(bridgeCoordinates, zoomLevel)
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' // this is the copyright info for the map
 }).addTo(map)
 
 bridges = [
@@ -13,46 +13,83 @@ bridges = [
     {"name": "George Washington Bridge", "Location": "New York, NY and New Jersey, NJ",	"span": 1067.0, "coordinates": [40.8517, -73.9527] },
     {"name": "Tacoma Narrows Bridge", "Location": "Tacoma and Kitsap, WA",	"span": 853.44, "coordinates": [47.2690, -122.5517] },
 ]
-/*
+
+// will obtain the bridge spans into an array
+let bridgeSpans = [] // empty array
+bridges.forEach(bridge => { //for every bridge
+    let bridgeSpan = bridge.span // get the span for that bridge
+    bridgeSpans.push(bridgeSpan) // add the span to the array
+})
+console.log("These are the bridge spans: ", bridgeSpans)
+
+let bridgeNames = [] 
+bridges.forEach(bridge => {
+    let bridgeName = bridge.name
+    bridgeNames.push(bridgeName)
+})
+console.log("These are the bridge names: ", bridgeNames)
+
+
 var bridgeIcon = L.icon({
     iconUrl: 'bridge.png',
     iconSize: [38, 95], // size of the icon 
     iconAchor: [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-});*/
+});
 
-bridges.forEach(function(bridgesMap){
-    let markerText = `<b>${bridgesMap.name}</b><br>${bridgesMap.span}`
-    L.marker([bridgesMap.coordinates]).bindPopup(markerText).addTo(map)
+var secondBridgeIcon = L.icon({
+    iconUrl: 'bridge2.png',
+    iconSize: [38, 95], // size of the icon 
+    iconAchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+//track the span - same as tracking the currency exchange rate value
+//track which bridge has that span - same way as you tracked the currency code
+let longestBridge
+let longestBridgeSpan = 0
+
+bridges.forEach(function(spanOfBridge) { // loop over each bridge in turn
+    if (spanOfBridge.span > longestBridgeSpan) { // if span is longer than any others we've seen so far
+        longestBridge = bridge // save this bridge in longestBridge variable
+        longestBridgeSpan = spanOfBridge.span // save the bridge's span in longestBridgeSpan variable for future comparisons
+    }
 })
-/*
-let longestBridge = 0
-if (bridges.span > longestBridge) {
-    longestBridge = bridges.span
-}
-console.log(longestBridge)
+console.log("Longest bridge", longestBridge)
 
-//{icon: bridgeIcon}
 
-let bridgeNameInput = document.querySelector('#bridge-name')
+bridges.forEach(function(oneBridge){
+    // if statement - is this the longest bridge? Draw special marker
+    // else, draw marker
+    if(oneBridge.span > longestBridge) {
+        let markerText = `<b>${oneBridge.name}</b><br>${oneBridge.span}` // adds the bridge name and span to each icon as a popup
+        L.marker(oneBridge.coordinates, {icon:bridgeIcon}).bindPopup(markerText).addTo(map) // added the bridgeicon image to the map
+    } else {
+        let markerText = `<b>${oneBridge.name}</b><br>${oneBridge.span}` // adds the bridge name and span to each icon as a popup
+        L.marker(longestBridge, {icon:secondBridgeIcon}).bindPopup(markerText).addTo(map) // added the bridgeicon image to the map
+    }
+})
+
+
+// create variable for an element in the html
+// all of the html is the 'document'
+let bridgeNameInput = document.querySelector('#bridge-name') 
 let spanLengthInput = document.querySelector('#span-length')
+let chartCanvas = document.querySelector('#bridge-bar-chart')
 
-let chartCanvas = document.querySelector('#bridge-doughnut-chart')
 let ctx = chartCanvas.getContext('2d')
 
 let bridgeChart = new Chart(ctx, {
-    type : 'doughnut',
+    type : 'bar',
     data: {
         datasets: [
             {
-            data: [bridges.name, bridges.span], // this will be the bridge data
-            backgroundColor: []
+            data: bridgeSpans, // this will be the bridge data
+            backgroundColor: ['tomato', 'orange', 'dodgerblue', 'mediumseagreen', 'slateblue', 'violet']
             }
         ],
-        labels: []
+        labels: bridgeNames
     },
     options: {}
 })
 
-let chartColors = [ 'tomato', 'orange', 'dodgerblue', 'mediumseagreen', 'slateblue', 'violet' ]
-*/
